c01dfdca78d0ff7203206ab8256ff04b0c15eb16
pwnsdx <sabri@wire.com>
2018-10-22 17:46:56 +0200

    Fix HMR

--- packages/desktop-updater-ui/stories/index.jsx
+++ packages/desktop-updater-ui/stories/index.jsx
@@ -2,6 +2,6 @@ import {Installer} from '../src/main/components/Installer';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-storiesOf('Installer')
+storiesOf('Installer', module)
   .add('0% loaded', () => <Installer />)
   .add('100% loaded', () => <Installer />);
