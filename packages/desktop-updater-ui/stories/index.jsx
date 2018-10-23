21ce6e3d4600380320a83d6a0ba80727021914eb
pwnsdx <sabri@wire.com>
2018-10-23 19:08:07 +0200

    Removed Installer "custom" story as anything can be custom now

--- packages/desktop-updater-ui/stories/index.jsx
+++ packages/desktop-updater-ui/stories/index.jsx
@@ -5,47 +5,73 @@ import React from 'react';
 import {WrapperOutdated} from '../src/main/components/WrapperOutdated';
 import {storiesOf} from '@storybook/react';
 
-const installerFakeData = {
-  elapsed: 8,
-  percent: 0.84,
-  remaining: 2,
-  speed: 736159,
-  startedAt: 1518433467150,
-  total: 7477716,
-  transferred: 6336123,
-};
+function installerStoryGenerator(data) {
+  const PERCENTAGE_MULTIPLY = 100;
+  const {progress, installing} = data;
+
+  return (
+    <Installer
+      installing={boolean('Are we installing?', installing)}
+      progress={{
+        elapsed: number('Elapsed time (seconds)', progress.elapsed),
+        percent:
+          number('Percentage (0-100)', progress.percent * PERCENTAGE_MULTIPLY) === 0
+            ? undefined
+            : progress.percent / PERCENTAGE_MULTIPLY,
+        remaining: number('Remaining time (in seconds)', progress.remaining),
+        speed: number('Current downloading speed (in bytes)', progress.speed),
+        startedAt: number('Started at (unix timestamp)', progress.startedAt),
+        total: number('Total size (in bytes)', progress.total),
+        transferred: number('Transferred size (in bytes)', progress.transferred),
+      }}
+    />
+  );
+}
 
 storiesOf('Installer', module)
   .addDecorator(withKnobs)
-  .add('Starting', () => <Installer />)
-  .add('In progress', () => <Installer installing={false} progress={installerFakeData} />)
-  .add('Finished', () => <Installer installing={true} progress={{...installerFakeData, percent: null}} />)
-  .add('Custom', () => {
-    const TOTAL_PERCENTAGE = 0;
-    const elapsed = number('Elapsed time (seconds)', installerFakeData.elapsed);
-    const installing = boolean('Are we installing?', true);
-    const percent = number('Percentage (0-100)', installerFakeData.percent * TOTAL_PERCENTAGE);
-    const remaining = number('Remaining time (in seconds)', installerFakeData.remaining);
-    const speed = number('Current downloading speed (in bytes)', installerFakeData.speed);
-    const startedAt = number('Started at (unix timestamp)', installerFakeData.startedAt);
-    const total = number('Total size (in bytes)', installerFakeData.total);
-    const transferred = number('Transferred size (in bytes)', installerFakeData.transferred);
-
-    return (
-      <Installer
-        installing={installing}
-        progress={{
-          elapsed,
-          percent: percent === 0 ? null : percent / TOTAL_PERCENTAGE,
-          remaining,
-          speed,
-          startedAt,
-          total,
-          transferred,
-        }}
-      />
-    );
-  });
+  .add('Starting', () =>
+    installerStoryGenerator({
+      installing: false,
+      progress: {
+        elapsed: 0,
+        percent: null,
+        remaining: undefined,
+        speed: 0,
+        startedAt: 0,
+        total: 0,
+        transferred: 0,
+      },
+    })
+  )
+  .add('In progress', () =>
+    installerStoryGenerator({
+      installing: false,
+      progress: {
+        elapsed: 8,
+        percent: 84,
+        remaining: 2,
+        speed: 736159,
+        startedAt: 1518433467150,
+        total: 7477716,
+        transferred: 6336123,
+      },
+    })
+  )
+  .add('Finished', () =>
+    installerStoryGenerator({
+      installing: true,
+      progress: {
+        elapsed: 8,
+        percent: null,
+        remaining: 2,
+        speed: 736159,
+        startedAt: 1518433467150,
+        total: 7477716,
+        transferred: 6336123,
+      },
+    })
+  );
 
 storiesOf('Prompt', module).add('New update is available', () => <Prompt />);
 
