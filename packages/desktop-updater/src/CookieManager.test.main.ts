d20fd0895d663dc6879e6f57998d3a51f98ab9e5
pwnsdx <sabri.haddouche@wire.com>
2019-04-05 11:01:29 +0200

    Added test for setting a cookie

--- packages/desktop-updater/src/CookieManager.test.main.ts
+++ packages/desktop-updater/src/CookieManager.test.main.ts
@@ -14,9 +14,16 @@ describe('CookieManager', () => {
   describe('set', () => {
     it('sets a cookie', async () => {
       const tempSession = session.fromPartition(new UUID(4).format());
-      const cookiesRaw = [];
-      const urlRaw = 'https://app.wire.com/';
+      const cookiesRaw = [
+        'aws-priv=eyJ2IjoxLCJldSI6MSwic3QiOjB9; Version=1; Comment="Anonymous cookie for privacy regulations"; Domain=.amazon.com; Max-Age=94672800; Expires=Tue, 05-Apr-2022 02:50:31 GMT; Path=/',
+        'aws_lang=en; Domain=.amazon.com; Path=/',
+      ];
+      const urlRaw = 'https://aws.amazon.com/';
       await CookieManager.set(cookiesRaw, urlRaw, tempSession);
+      assert.strictEqual(
+        await CookieManager.get(urlRaw, tempSession),
+        'aws-priv=eyJ2IjoxLCJldSI6MSwic3QiOjB9; aws_lang=en; '
+      );
     });
   });
 });
