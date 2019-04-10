d010739895668d2fa003d29872724f7594363355
Florian Keller <florian.keller@wire.com>
2019-04-10 15:27:14 +0200

    fix(build-tools): Write productName when building

--- packages/build-tools/src/cli/build-windows-cli.ts
+++ packages/build-tools/src/cli/build-windows-cli.ts
@@ -21,6 +21,7 @@
 
 import commander from 'commander';
 import electronPackager from 'electron-packager';
+import * as fs from 'fs-extra';
 import * as path from 'path';
 
 import {checkCommanderOptions, getLogger, getToolName, writeJson} from '../lib/build-utils';
@@ -39,6 +40,8 @@ checkCommanderOptions(commander, ['wireJson']);
 
 const wireJsonResolved = path.resolve(commander.wireJson);
 const {commonConfig, defaultConfig} = getCommonConfig({envFile: '.env.defaults', wireJson: wireJsonResolved});
+const electronPackageJson = path.resolve(commonConfig.electronDirectory, 'package.json');
+const originalElectronJson = fs.readJsonSync(electronPackageJson);
 
 const packagerOptions: electronPackager.Options = {
   appCopyright: commonConfig.copyright,
@@ -68,10 +71,13 @@ logEntries(commonConfig, 'commonConfig', toolName);
 
 logger.info(`Building ${commonConfig.name} ${commonConfig.version} for Windows ...`);
 
-writeJson(wireJsonResolved, commonConfig)
+writeJson(electronPackageJson, {...originalElectronJson, productName: commonConfig.name, version: commonConfig.version})
+  .then(() => writeJson(wireJsonResolved, commonConfig))
   .then(() => electronPackager(packagerOptions))
   .then(([buildDir]) => logger.log(`Built package in "${buildDir}".`))
-  .finally(() => writeJson(wireJsonResolved, defaultConfig))
+  .finally(() =>
+    Promise.all([writeJson(wireJsonResolved, defaultConfig), writeJson(electronPackageJson, originalElectronJson)])
+  )
   .catch(error => {
     logger.error(error);
     process.exit(1);
